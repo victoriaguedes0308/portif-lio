0a284437d39ab36a9437902da71d1ee17a72dd2c
victoriaguedes0308 <guedesvictoria01@gmail.com>
2026-02-10 07:54:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
